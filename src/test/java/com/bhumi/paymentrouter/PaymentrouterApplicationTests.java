eccfbffa59a29b91c98232a27d9e82175d63f208
Bhumi Singh <singhbhumi0927@gmail.com>
2026-06-01 19:13:43 +0530

    Spring Boot setup completed

---
+++ src/test/java/com/bhumi/paymentrouter/PaymentrouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhumi.paymentrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentrouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
