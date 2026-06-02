c4a2b4fa63a4a22a6aed664b1bc7ab4750ccf9bc
Bhumi Singh <singhbhumi0927@gmail.com>
2026-06-03 00:01:25 +0530

    fix cors

--- src/main/java/com/bhumi/paymentrouter/config/CorsConfig.java
+++ src/main/java/com/bhumi/paymentrouter/config/CorsConfig.java
@@ -1,20 +1,28 @@
 package com.bhumi.paymentrouter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "http://localhost:5174")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://smart-payment-router.vercel.app",
+                            "https://smart-payment-router-*.vercel.app"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
     }
 }
